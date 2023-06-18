6144b004057e113f6bd1b533212a819a859c6d05
unknown <abdullah.k.alakel@gmail.com>
2023-06-18 16:28:57 +0200

    initiate two .js files instead of(script.js)and connect each to the related html page, also made the two buttons inside index.html page working as expected using javascrupt inside(startscript.js)file

---
+++ assets/js/startscript.js
@@ -0,0 +1,13 @@
+// Declare variables and set the two buttons inside start page to these variables
+let startBtn = document.getElementById("start-btn"),
+    exitBtn = document.getElementById("first-exit-btn");
+
+// Add event listener (click) that proceed with a function to open the next app page when the user clicked on the Start button
+startBtn.addEventListener("click", function () {
+    window.open("todolistpage.html", "_self");
+});
+// Add event listener (click) that proceed with a function to close the app when the user clicked on the Exit button
+exitBtn.addEventListener("click", function () {
+    window.open("seeyoulater.html", "_self");
+    window.close();
+});
