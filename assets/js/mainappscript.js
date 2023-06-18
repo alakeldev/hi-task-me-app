847ede7a8d9c6c6144a56472d492e52bea9dbe64
unknown <abdullah.k.alakel@gmail.com>
2023-06-19 01:04:32 +0200

    Creat Empty Array to Store new tasks, creat a function that will work on add-new-task button clicked event, another function to give each new task a random id also to add it to theAlltasksArray

--- assets/js/mainappscript.js
+++ assets/js/mainappscript.js
@@ -9,3 +9,25 @@ on the Exit button and lead the user to (see you later page) */
 exitBtn.addEventListener("click", function () {
     window.open("seeyoulater.html", "_self");
 });
+
+// Creat new Empty Array to Add and Store the new Tasks one after one
+let theAllTasksArray = [];
+
+// addNewTask button -  Anonymous Function will proceed when the user click on the add new task button
+addTaskBtn.onclick = function () {
+    if (taskInput !== "") {            // if condition checking the new task input that is not empty 
+        myNewTasks(taskInput.value);   // Call function that take the user input value ( new task) as a parameter
+        taskInput.value = "";          // to Empty the input field after taking its value related to code in previous line
+    }
+}
+
+// Creat a function that takes the user input (new task) and add it to the empty array theAllTasksArray
+function myNewTasks(theCtask) {
+    // Creat object to Hold the Task Data and give each one a random ID
+    let newTaskData = {
+        id: Math.floor(Math.random() * 1e7),             // the ID random number
+        content: theCtask,                               // the new task content (it comes from the new task input field(value))
+    };
+    // Add the new Task to the array (theAllTasksArray) and store it with its random ID
+    theAllTasksArray.push(newTaskData);
+}
